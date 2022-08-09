6e84f2e75f5553e5c00a986d0d6eb5b9ca247051
鈴山佳宏 <suzusanyoshihiro@admin-2.local>
2022-08-09 22:51:52 +0900

    2/fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,24 @@ import axios from "axios";
 
 function App() {
 	const [data, setData] = React.useState();
+	const [data2, setData2] = React.useState();
 	const url = "http://127.0.0.1:8000";
+        const url2 = "https://api.github.com/users/efficient-life/repos";
+
 
 	const GetData = () => {
 		axios.get(url).then((res) => {
 			setData(res.data);
+		});
+		axios.get(url2).then((res) => {
+			setData2(res.data);
 		});
 	};
 	return (
 		<div>
 			<div>ここに処理を書いていきます</div>
 			{data ? <div>{data.Hello}</div> : <button onClick={GetData}>データを取得</button>}
+                        {data2}
 		</div>
 	);
 }
