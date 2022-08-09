3f653657a07691a963dd87b37ddbca578892d224
鈴山佳宏 <suzusanyoshihiro@admin-2.local>
2022-08-09 23:38:30 +0900

    app.jsx/fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,18 @@ import axios from "axios";
 
 function App() {
 	const [data, setData] = React.useState();
-	const [data2, setData2] = React.useState();
 	const url = "https://tranquil-chamber-13279.herokuapp.com/";
-        const url2 = "https://api.github.com/users/efficient-life/repos";
-
 
 	const GetData = () => {
 		axios.get(url).then((res) => {
 			setData(res.data);
 		});
-		axios.get(url2).then((res) => {
-			setData2(res.data);
-		});
 	};
 	return (
 		<div>
 			<div>ここに処理を書いていきます</div>
-			{data ? <div>{data.Hello}</div> : <button onClick={GetData}>データを取得</button>}
-                        {data2}
+			{data && <div>{data.Hello}</div>}
+			<button onClick={GetData}>データを取得</button>
 		</div>
 	);
 }
